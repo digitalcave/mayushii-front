8eae9fe3e144ca8a63aea38018a6286e9db98bd1
Santiago Lorenzo <43711998+SantyLorenzo@users.noreply.github.com>
2020-05-05 00:59:23 -0300

    Feature/014 - FE - Add basic image and pet sound (#1)
    * React-sound installed
    
    * mayoshi image
    
    * Tuturu sound
    
    * Antd styles imported globally
    
    * Home mockup, image and sound button

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+// @ts-ignore 
+import Sound from 'react-sound';
+import { Button as Btn } from 'antd';
 import styled from "@emotion/styled";
 
-const Home = () => <Wrapper>Tuturu :D</Wrapper>;
+const Home = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleOnClick = () => setIsPlaying(!isPlaying);
+
+  const handleSoundFinished = () => setIsPlaying(false);
+
+  return (
+    <Wrapper>
+      <Img src='images/mayoshi.png' alt='mayoshii' />
+      <ButtonsContainer>
+        <Sound
+          url='sounds/tuturu.mp3'
+          playStatus={Sound.status[isPlaying ? 'PLAYING' : 'STOPPED']}
+          onFinishedPlaying={handleSoundFinished}
+        />
+        <Button onClick={handleOnClick} type="primary">TUTURU</Button>
+      </ButtonsContainer>
+    </Wrapper>
+  );
+};
 
 export default Home;
 
 const Wrapper = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Img = styled.img`
+  width: 20vw;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled(Btn)`
+  margin: 3vw;
 `;
